cef7af41af55fca0c548d8d13aee522681b49023
Mihail <kostochkin2002@inbox.ru>
2023-04-24 21:25:04 +0700

    Вынесение js скрипта в отдельный файл

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    $('.Gallery').slick({
-      setting-name: setting-value
-    });
+  $('.Gallery').slick({
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+      nextArrow: '<button type="button" class="slick-next">Next</button>',
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      centerMode: true,
+      variableWidth: true
   });
+});
